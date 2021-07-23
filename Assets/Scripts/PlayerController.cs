86493f3a32f5a41938936f033edb143707bdfd81
George Anderson <79677044+ShowMeTheMorty@users.noreply.github.com>
2021-07-24 10:23:24 +1200

    Hey Homies, Player Initial Commit
    Not yet sensible

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveAcceleration;
+    public float moveSpeedMax;
+    public float jumpHeight;
+    public float footDepth;
+
+    private Rigidbody2D rb;
+    private bool onGround;
+    private RaycastHit2D footRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CastFeet();
+        CharacterPrimaryControl();
+
+    }
+
+    private void CastFeet()
+    {
+        RaycastHit2D hit;
+        //if (Physics2D.Raycast(transform.position, new Vector2(0f, -1f), footDepth, out hit, )
+    }
+
+    private void CharacterPrimaryControl()
+    {
+        float move = Input.GetAxis("Horizontal");
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+        if (Mathf.Abs(rb.velocity.x) < moveSpeedMax) rb.velocity += new Vector2(move * moveAcceleration, 0f);
+        if (jump) rb.velocity += new Vector2(0f, 1f) * jumpHeight;
+    }
+}
