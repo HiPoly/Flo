5fb499b4e4371b1bc750a1bc3c60f0bd55110e68
HiPoly <61814399+HiPoly@users.noreply.github.com>
2021-07-24 16:05:09 +1200

    Added Interact Prompts
    Interact Script to be stolen from and some UI Improvements

---
+++ Assets/Scripts/Prompts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prompts : MonoBehaviour
+{
+    public GameObject PromptCanvas;
+    public GameObject LockedIcon;
+    public GameObject UnlockedIcon;
+    
+    private bool touching;
+    private bool locked = false;
+    //if player is within range of the object set touching to true
+
+void Update(){
+    if (touching){
+        PromptCanvas.SetActive(true);
+
+        if (locked){
+            LockedIcon.SetActive(true);
+            }
+        else{
+            LockedIcon.SetActive(false);
+        }
+        if(!locked){
+            UnlockedIcon.SetActive(true);
+        }
+        else
+            UnlockedIcon.SetActive(false);
+        }
+        else{
+            PromptCanvas.SetActive(false);
+        }
+    }
+}
